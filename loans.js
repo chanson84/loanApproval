ae809d7a36eebfe1bb7b35ca1bb58c0007d71836
Chris <chansonart84@gmail.com>
2020-11-22 15:47:14 -0600

    Initial commit

---
+++ loans.js
@@ -0,0 +1,23 @@
+$(document).ready(
+    function () {
+
+//add event handlers
+var myRules ={
+    salary:{required:true,
+    min:0,
+    max:100000,
+    digits:true},
+    creditScore:{required:true,
+    min: 300,
+    max: 850,
+    digits: true
+    },
+    jobTime:{required:true,
+    min:0,
+    max:360,
+    digits:true}
+}
+        //all other functions (program logic)
+
+    }
+)
